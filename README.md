3a150c67bca91f9dbd8eefbd9723081cb252be4a
bestime <1174295440@qq.com>
2019-07-01 14:20:59 +0800

    new

---
+++ README.md
@@ -0,0 +1,71 @@
+
+
+
+#### 参数详解
+
+| 参数        | 描述  | 
+| --------   | -----:  |
+| on      | 事件注册   |
+| emit      | 事件触发  |
+
+#### 安装
+```
+// 使用 npm
+npm i create-bus
+const CreateEventBus = require('create-event-bus'); // 引入方式01
+import CreateEventBus from 'create-event-bus'; // 引入方式02
+
+// 使用 cdn (版本可选 @xx.xx.xx)
+<script src="https://cdn.jsdelivr.net/npm/create-event-bus@latest/create-event-bus.min.js"></script>
+
+```
+
+#### 第一步 - 实例化
+```javascript
+const bus = new CreateEventBus() // 实例化bus
+```
+
+#### 第二步 - 注册
+```javascript
+// 注册永久事件
+var demo01 = bus.on('test01', function (res) {
+  console.log(res)
+})
+
+// 注册永久事件
+var demo02 = bus.on('test01', function (res) {
+  console.log(res)
+})
+
+// 注册一次性事件
+var demo03 = bus.once('test01', function (res) {
+  console.log(res)
+})
+
+// 注册永久唯一事件, 将会覆盖名为 test01 的函数
+var demo04 = bus.one('test01', function (res) {
+  console.log(res)
+})
+
+// 注册一次性唯一事件, 将会覆盖名为 test01 的函数
+var demo05 = bus.oneonce('test01', function (res) {
+  console.log(res)
+})
+```
+#### 第三步 - 事件触发
+```javascript
+bus.emit('test01', {
+  msg: '这是附带的数据',
+  data: [1, 2, 3, 4]
+})
+```
+
+#### 清空bus实例（页面销毁前或者其他有需要得场景使用）
+```javascript
+bus.clear(demo01)
+bus.clear(demo02)
+bus.clear(demo03)
+bus.clear(demo04)
+bus.clear(demo05)
+```
+
---
+++ create-event-bus.js
@@ -0,0 +1,125 @@
+// createBus 2019-06-29
+
+;(function (global) {
+  // 导出方法
+  function _export (global, name, handle) {
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+      module.exports = handle;
+    } else if (typeof define === 'function' && define.amd) {
+      define([], function () {
+        return handle;
+      });
+    } else {
+      global[name] = handle;
+    }
+  }
+
+  // 获取数据类型
+  function getType (data) {
+    return Object.prototype.toString.call(data).split(' ')[1].slice(0, -1)
+  }
+
+  function Bus () {
+    this.events = {}; // 事件存放中心
+    this.autoId = 0; // 自动生成的 id
+  }
+
+  // 基础注册函数
+  Bus.prototype.create = function (customeid, name, handle, isOnce, onlyOne) {
+    if(getType(handle) !== 'Function') return null;
+    var useId = customeid ? customeid : this.autoId++
+    var oneHandle = {      
+      name: name,
+      id: useId,
+      handle: handle,
+      isOnce: isOnce===true ? true : false
+    };
+
+    if(onlyOne) {
+      this.events[name] = oneHandle
+    } else {
+      getType(this.events[name]) !== 'Array' && (this.events[name] = []);
+      var isFind = false
+      for(var a = 0; a< this.events[name].length; a++) {
+        if(this.events[name][a].id === useId) {
+          this.events[name][a] = oneHandle
+          isFind = true
+          break;
+        }
+      }
+      !isFind && this.events[name].push(oneHandle)
+    }    
+    return oneHandle
+  }
+
+  // 事件触发
+  Bus.prototype.emit = function (name, data) {    
+    switch (getType(this.events[name])) {
+      case 'Object':
+        this.events[name].handle(data, this.events[name])
+        if(this.events[name].isOnce) {
+          delete this.events[name]
+        }
+        break;
+      case 'Array':
+        for(var a=this.events[name].length-1; a>=0; a--) {
+          var item = this.events[name][a]
+          item.handle(data, item)
+          if(item.isOnce) {
+            this.events[name].splice(a, 1)
+          }
+        }
+        break;
+    }
+  }
+
+  // 清空整个 bus
+  Bus.prototype.clearAll = function () {
+    this.events = {}
+    this.autoId = 0
+  }
+
+  // 清空一个 bus 实例
+  Bus.prototype.clear = function (opt) {
+    if(getType(opt) !== 'Object') return;
+    switch (getType(this.events[opt.name])) {
+      case 'Object':
+        delete this.events[opt.name]
+        break;
+      case 'Array':
+        for(var a = this.events[opt.name].length-1; a>=0; a--) {
+          if(this.events[opt.name][a].id===opt.id) {
+            this.events[opt.name].splice(a, 1)
+          }
+        }
+        break;
+    }
+  }
+
+  // 【注册】 永久函数
+  Bus.prototype.on = function (name, handle, id) {
+    return this.create(id, name, handle)    
+  }
+
+  // 【注册】 一次性函数
+  Bus.prototype.once = function (name, handle, id) {
+    return this.create(id, name, handle, true)
+  }
+
+  // 【注册】 永久唯一函数
+  Bus.prototype.one = function (name, handle) {
+    return this.create(null, name, handle, false, true)
+  }
+
+  // 【注册】 一次性唯一函数
+  Bus.prototype.oneonce = function (name, handle) {    
+    return this.create(null, name, handle, true, true)
+  }
+
+  // 导出函数
+  _export(global, 'CreateEventBus', Bus)
+}(this));
+
+
+
+
